eea0378c14d21c1f243254de0982d7247a2c9710
Henauxg <gill.henaux@gmail.com>
2025-05-27 16:53:46 +0200

    feat: checksum

--- src/main.rs
+++ src/main.rs
@@ -16,31 +16,69 @@ fn parse_csv_from_stdin() -> (usize, usize) {
     };
     let headers: Vec<&str> = headers_line.split(',').collect();
     // Find the "ean" header
-    let Some(ean_column_index) = headers.iter().position(|c| *c == "ean") else {
-        // TODO missing_ean_column expects invalid count to be the number of lines in the file
-        return (0, lines.count() + 1);
+    let ean_column_index = match headers.iter().position(|c| *c == "ean") {
+        Some(column_index) => column_index,
+        // "If the header is missing, it will be the first column"
+        None => 0,
     };
 
     let mut valid_count = 0;
     let mut invalid_count = 0;
 
-    // TODO Parse content
+    // Parse content
     while let Some(line) = lines.next() {
         let Ok(line) = line else {
             // TODO Should it be considered an error on the whole file ?
             invalid_count += 1;
             continue;
         };
+        if line.is_empty() {
+            // "If the line is empty, skip it"
+            continue;
+        }
         let columns: Vec<&str> = line.split(',').collect();
-        // TODO Could/should consider that missing columns (not just EAN) is an error ?
-        let Some(_ean) = columns.get(ean_column_index) else {
+        let Some(ean_str) = columns.get(ean_column_index) else {
             invalid_count += 1;
             continue;
         };
+        // "Quoted EANs should be considered as valid if the EAN itself is"
+        let ean = ean_str.trim_matches('"');
 
-        // TODO Verify EAN
-        valid_count += 1;
+        match is_valid_gtin_13(ean) {
+            true => valid_count += 1,
+            false => invalid_count += 1,
+        };
     }
 
     (valid_count, invalid_count)
+}
+
+fn is_valid_gtin_13(_ean: &str) -> bool {
+    // TODO
+    false
+}
+
+fn gtin_13_checksum(ean: Vec<u8>) -> Option<u8> {
+    if ean.len() < 13 {
+        return None;
+    }
+    let sum = ean[0]
+        + 3 * ean[1]
+        + ean[2]
+        + 3 * ean[3]
+        + ean[4]
+        + 3 * ean[5]
+        + ean[6]
+        + 3 * ean[7]
+        + ean[8]
+        + 3 * ean[9]
+        + ean[10]
+        + 3 * ean[11];
+
+    let checksum = match sum % 10 {
+        0 => 0,
+        n => 10 - n,
+    };
+
+    Some(checksum)
 }
